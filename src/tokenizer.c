787eb1a46cb26bdd946d30b09dccbb61ddaa8845
Miguel Camarillo <macamarilloelizondo@miners.utep.edu>
2020-09-21 21:12:00 -0600

    Added the Tokenizer.c blank

---
+++ src/tokenizer.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int space_char(char c)
+{
+  return(0);
+}
+
+int non_space_char(char c)
+{
+  return(0);
+}
+
+char *word_start(char *str)
+{
+  return(*str); 
+}
+
+char *word_terminator(char *word)
+{
+  return(*word);
+}
+
+int count_words(char *str)
+{
+  return(0);
+}
+
+char *copy_str(char *inStr, short len)
+{
+  return(*inStr);
+}
+
+char **tokenize(char* str)
+{
+  return(*str);
+}
+
+void print_tokens(char **tokens)
+{
+  return();
+}
+
+void free_tokens(char **tokens)
+{
+  return();
+}
