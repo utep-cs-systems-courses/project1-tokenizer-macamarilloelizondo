f3c7db32f6431935602aab6598c6b27e1fb3328d
Miguel Camarillo <macamarilloelizondo@miners.utep.edu>
2020-09-21 22:08:46 -0600

    Added the history.c file in blank

---
+++ src/history.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+List *init_history()
+{
+  
+}
+
+void add_history(List *list, char *str)
+{
+
+}
+
+char *get_history(List *list, int id)
+{
+
+}
+
+void print_history(List *list)
+{
+  
+}
+
+void free_history(List *list)
+{
+
+}
